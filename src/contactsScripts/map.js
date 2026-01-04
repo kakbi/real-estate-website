41bfac8ea32530471e0afcfdcefff1da6c900c15
Vadym Chebotar <dramvoda@gmail.com>
2026-01-05 01:39:59 +0200

    Add environment-specific path handling for local and GitHub Pages

--- src/contactsScripts/map.js
+++ src/contactsScripts/map.js
@@ -1,14 +1,21 @@
 import { fetchData } from '../utils/fetchData.js';
 import { initMenu } from '../utils/menu.js';
 import { setIcon } from './mapIcon.js';
+
+const isGitHubPages = window.location.hostname === 'kakbi.github.io';
+
+const basePath = isGitHubPages ? '/real-estate-website' : ''; // Для GitHub Pages указываем репозиторий, локально оставляем пустым
+
 document.addEventListener('DOMContentLoaded', async () => {
-    const data = await fetchData('../../data/data.json');
+    // const data = await fetchData('../../data/data.json');
+    const data = await fetchData(`${basePath}/data/data.json`);
 
     var map = L.map('map').setView([49.01464593084217, 31.252700508048274], 6);
 
     data.forEach((obj) => {
         L.marker([obj.latitude, obj.longitude], {
-            icon: setIcon('../data/images/iconHouse.png', [36, 36]),
+            // icon: setIcon('../data/images/iconHouse.png', [36, 36]),
+            icon: setIcon(`${basePath}/data/images/iconHouse.png`, [36, 36]),
         })
             .addTo(map)
             .bindPopup(
@@ -17,7 +24,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
 
     L.marker([46.484457320538084, 30.734480080285376], {
-        icon: setIcon('../data/images/iconOffice.png', [56, 56]),
+        // icon: setIcon('../data/images/iconOffice.png', [56, 56]),
+        icon: setIcon(`${basePath}/data/images/iconOffice.png`, [56, 56]),
     })
         .addTo(map)
         .bindPopup(
